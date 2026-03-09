3d98b1e289ef59591fffb621417600dd9f1eba12
RB-Bravo <rene.boy.bravo@student.pnm.edu.ph>
2026-03-09 15:52:59 +0800

    peak hour service

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,7 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
 import { UsersModule } from './users/users.module';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PeakHourModule } from './peak-hour/peak-hour.module';
 
 @Module({
   imports: [
@@ -19,6 +20,7 @@ import { AppService } from './app.service';
       }),
     }),
     UsersModule,
+    PeakHourModule,
   ],
   controllers: [AppController],
   providers: [AppService],
