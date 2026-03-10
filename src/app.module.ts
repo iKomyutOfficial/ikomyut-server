2284c8641c8eb874288d5acadd8fb39de5960114
timothyjohnnepacena <timothy.john.nepacena@student.pnm.edu.ph>
2026-03-10 13:17:41 +0800

    crud time-matrix

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,6 +6,7 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DriversModule } from './driver/driver.module';
 import { AdminModule } from './admin/admin.module';
+import { TimeMatrixModule } from './time-matrix/time-matrix.module';
 
 @Module({
   imports: [
@@ -23,6 +24,7 @@ import { AdminModule } from './admin/admin.module';
     UsersModule,
     DriversModule,
     AdminModule,
+    TimeMatrixModule,
   ],
   controllers: [AppController],
   providers: [AppService],
