d4e410b5c72a2c6e3f35b4351090c3f9f37c5484
rodjean015 <rodjeanverzosa001@gmail.com>
2026-03-11 08:53:48 +0800

    fix merge conflict on app module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,14 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
 import { UsersModule } from './users/users.module';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PeakHourModule } from './peak-hour/peak-hour.module';
+import { AdminModule } from './admin/admin.module';
+import { DriversModule } from './driver/driver.module';
+import { PaymentTypeModule } from './payment-type/payment-type.module';
+import { RainyDaySurchargeModule } from './rainy-day-surcharge/rainy-day-surcharge.module';
+import { TimeMatrixModule } from './time-matrix/time-matrix.module';
+import { TnvsConfigurationModule } from './tnvs-configuration/tnvs-configuration.module';
+import { TrafficIntensityModule } from './traffic-intensity/traffic-intensity.module';
 
 @Module({
   imports: [
@@ -19,6 +27,14 @@ import { AppService } from './app.service';
       }),
     }),
     UsersModule,
+    DriversModule,
+    AdminModule,
+    TimeMatrixModule,
+    RainyDaySurchargeModule,
+    PaymentTypeModule,
+    TnvsConfigurationModule,
+    TrafficIntensityModule,
+    PeakHourModule
   ],
   controllers: [AppController],
   providers: [AppService],
