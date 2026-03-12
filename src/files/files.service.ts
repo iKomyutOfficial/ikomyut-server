8e4f54de1d021023c8f7a08928a545692fad89f9
rodjean015 <rodjeanverzosa001@gmail.com>
2026-03-12 08:58:10 +0800

    configure AWS sdk aws bucket

---
+++ src/files/files.service.ts
@@ -0,0 +1,60 @@
+import { Injectable } from '@nestjs/common';
+import {
+  S3Client,
+  PutObjectCommand,
+  GetObjectCommand,
+} from '@aws-sdk/client-s3';
+import { getSignedUrl } from '@aws-sdk/s3-request-presigner';
+import { ConfigService } from '@nestjs/config';
+
+@Injectable()
+export class FilesService {
+  private s3: S3Client;
+
+  constructor(private readonly configService: ConfigService) {
+    this.s3 = new S3Client({
+      region: this.configService.get<string>('AWS_REGION'),
+      credentials: {
+        accessKeyId: this.configService.get<string>('AWS_ACCESS_KEY_ID')!,
+        secretAccessKey: this.configService.get<string>(
+          'AWS_SECRET_ACCESS_KEY',
+        )!,
+      },
+    });
+  }
+
+  /**
+   * Upload file to S3
+   */
+  async uploadFile(dataBuffer: Buffer, filename: string, mimetype?: string) {
+    const command = new PutObjectCommand({
+      Bucket: this.configService.get('AWS_PUBLIC_BUCKET_NAME'),
+      Key: filename,
+      Body: dataBuffer,
+      ContentType: mimetype,
+    });
+
+    await this.s3.send(command);
+
+    return {
+      key: filename,
+      bucket: this.configService.get('AWS_PUBLIC_BUCKET_NAME'),
+    };
+  }
+
+  /**
+   * Generate pre-signed URL
+   */
+  async getFileUrl(filename: string): Promise<string> {
+    const command = new GetObjectCommand({
+      Bucket: this.configService.get('AWS_PUBLIC_BUCKET_NAME'),
+      Key: filename,
+    });
+
+    const url = await getSignedUrl(this.s3, command, {
+      expiresIn: 3600, // 1 hour
+    });
+
+    return url;
+  }
+}
